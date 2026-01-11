2264998611599ec463ac1da8558fc8f385744d8a
Jahnavi <gaurjahnavi84@gmail.com>
2026-01-11 23:16:35 +0530

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,30 @@
 // const express = require("express")
 import express from "express"
+import path from "path"
 import { ENV } from "./lib/env.js"
 
 const app = express()
 
-app.get('/',(req,res)=>{
+const __dirname = path.resolve()
+
+app.get('/health',(req,res)=>{
     res.status(200).json({msg:"success from api backend 12345"})
 })
+
+app.get('/books',(req,res)=>{
+    res.status(200).json({msg:"this is books end point"})
+})
+
+
+
+//make our app ready for deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
+    })
+}
 
 
 app.listen(ENV.PORT,()=>{
