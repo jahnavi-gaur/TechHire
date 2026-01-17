1f2ed1b92b27e38e2328e9ff0e2680ee772d3707
Jahnavi <gaurjahnavi84@gmail.com>
2026-01-17 10:08:16 +0530

    auth and db setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,23 @@
 // const express = require("express")
 import express from "express"
 import path from "path"
+import cors from "cors"
+import {serve} from "inngest/express"
+
 import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js"
+import { inngest, functions } from "./lib/inngest.js"
 
 const app = express()
 
 const __dirname = path.resolve()
+
+//middleware
+app.use(express.json())
+//credentials:true meaning -> sserver allows a browser to include cookies on request
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))
+
+app.use("/api/inngest", serve({client: inngest, functions }))
 
 app.get('/health',(req,res)=>{
     res.status(200).json({msg:"success from api backend 12345"})
@@ -26,8 +37,6 @@ if(ENV.NODE_ENV === "production"){
         res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
     })
 }
-
-
 
 
 const startServer = async()=>{
